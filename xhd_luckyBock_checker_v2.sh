7f128f2675584209f1b2cd29095e719d34ef03bd
Randy Grolemund <randygrolemund@Randys-MacBook-Pro.local>
2019-12-07 09:18:27 -0600

    added last payment

--- xhd_luckyBock_checker_v2.sh
+++ xhd_luckyBock_checker_v2.sh
@@ -3,27 +3,34 @@
 # XHD Lucky block analyzer. Created by BearlyHealz.
 
 # Blockchain Explorer URL
-blockExplorer_url='https://explorer.xrphd.org/explorer/api/blockchain/network'
+blockExplorer_url='https://explorer.xrphd.org/explorer/api/blockchain'
+
 
 # Get data from explorer API
-curl $blockExplorer_url -s -k -X GET –header Content-Type: application/json’ > network_explorer.json
+curl $blockExplorer_url/network -s -k -X GET –header Content-Type: application/json’ > network_explorer.json
 
 # Parse accumulatesubsidy (overage)
 get_accumulateSubsidy=$(jq '.accumulatesubsidy' network_explorer.json | tr -d '"' )
 
+# Parse last block hash
+get_lastBlockHash=$(jq '.best.hash' network_explorer.json | tr -d '"' )
+
+# Get last payment from API
+lastPayment=$(curl $blockExplorer_url/block/$get_lastBlockHash/transaction  -s -k -X GET –header Content-Type: application/json | jq '.data[0].vout[0].amount' | tr -d '"')
+
 # Remove decimal
 get_accumulateSubsidy=${get_accumulateSubsidy%.*}
 
-        # Check if next payment will be lucky
-        if [[ $get_accumulateSubsidy > '1' ]]
-        then
+# Check if next payment will be lucky
+if [[ $get_accumulateSubsidy > '1' ]]
+    then
             # Calculate the next reward
             estimatedReward=$((get_accumulateSubsidy+150000))
-            echo "Next block IS a lucky block! The estimated next reward should be $estimatedReward XHD."
+            echo "Next block IS a lucky block! The estimated next reward should be $estimatedReward XHD. Last reward: $lastPayment XHD."
 
-        else
-            echo "Next block IS NOT a lucky block!"
+    else
+            echo "Next block IS NOT a lucky block! Last reward: $lastPayment XHD."
 
-        fi
+ fi
 
 
