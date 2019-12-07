d9b13013ef27638289b08829ccea9c9711f7336f
Randy Grolemund <randygrolemund@Randys-MacBook-Pro.local>
2019-12-07 08:16:02 -0600

    working version

--- xhd_luckyBock_checker_v2.sh
+++ xhd_luckyBock_checker_v2.sh
@@ -6,16 +6,17 @@
 blockExplorer_url='https://explorer.xrphd.org/explorer/api/blockchain/network'
 
 # Parse accumulatesubsidy (overage)
-get_accumulateSubsidy=$(curl $blockExplorer_url -s -k -X GET –header Content-Type: application/json’ | jq '.accumulatesubsidy' | tr -d '"')
+get_accumulateSubsidy=$(curl $blockExplorer_url -s -k -X GET –header Content-Type: application/json’ | jq '.accumulatesubsidy' | tr -d '"' | bc)
+get_accumulateSubsidy=${get_accumulateSubsidy%.*}
 
         # Check if next payment will be lucky
-        if [[ get_accumulateSubsidy > '0.00000000' ]]
+        if [[ $get_accumulateSubsidy > '1' ]]
         then
-            echo "Next block IS NOT a lucky block!"
-
-        else
             estimatedReward=$((get_accumulateSubsidy+150000))
             echo "Next block IS a lucky block! The estimated next reward should be $estimatedReward XHD."
+
+        else
+            echo "Next block IS NOT a lucky block!"
 
         fi
 
